f13991f1ec352a62f2f0fad48088b7de559d8de2
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-09 16:41:13 -0300

    Primeiro contato com enumeracao

---
+++ enum1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace enum1.Entities.Enums
+{
+    public enum OrderStatus: int
+    {
+        PendingPayment = 0,
+        Processing =1,
+        Shipped = 2,
+        Delivered = 3
+
+    }
+}
---
+++ enum1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using enum1.Entities;
+using enum1.Entities.Enums;
+
+namespace enum1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = (OrderStatus) Enum.Parse(typeof(OrderStatus), "Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
